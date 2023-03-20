4284d93bbd81d65dde108940523566b01dc52390
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-03-20 15:49:54 +0200

    First step of creating ChatApp

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿using Chat_app2.dtos;
+using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System.Threading.Tasks;
+
+namespace Chat_app2.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        [HttpPost("messages")]
+        public async Task<ActionResult> Message(MessageDTO dto)
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "eu",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+              "1565118",
+              "3cf6710fe0762cade304",
+              "72e04164755a97b21e76",
+              options);
+
+            await pusher.TriggerAsync(
+              "chat",
+              "message",
+              new { 
+                  username = dto.Username,
+                  message = dto.Message 
+              });
+            return Ok(new string[] { });
+
+
+        }
+    }
+}
